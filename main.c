e73538112b5f16b627bef973737f02d7f0dccf8e
yahyaHadar <yahyahadar@gmail.com>
2023-09-22 09:57:31 -0700

    commit

--- main.c
+++ main.c
@@ -23,20 +23,26 @@ int main(int argc, char **argv)
 		if (line == NULL)
 		{
 			if (isatty(STDIN_FILENO))
+			{
 				write(STDOUT_FILENO, "\n ", 1);
+			}
 			return (status);
 		}
 		idx++;
-
 		command = tokenizer(line);
+
 		if (!command)
+		{
 			continue;
-
+		}
 		if (is_builtin(command[0]))
+		{
 			handle_builtin(command, argv, &status, idx);
-
+		}
 		else
+		{
 			status = _execute(command, argv, idx);
+		}
 	}
 
 }
